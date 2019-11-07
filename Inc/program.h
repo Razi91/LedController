cb3e18e140c1143545cac729a483b89daa1536d2
jkonieczny <razi91@o2.pl>
2019-11-07 19:25:41 +0100

    refactor

--- Inc/program.h
+++ Inc/program.h
@@ -21,6 +21,18 @@ typedef struct Status {
 
 extern Status status;
 
+union Command {
+	uint8_t data[PIXELS * 3 + 1];
+	struct {
+		uint8_t pixels;
+		ws2812b_color colors[PIXELS];
+	};
+}__attribute__((packed));
+
+
+#define WS_ZERO 0b11000000
+#define WS_ONE 0b11111000
+
 #ifdef __cplusplus
 }
 #endif
--- Src/program.cpp
+++ Src/program.cpp
@@ -7,23 +7,12 @@ extern SPI_HandleTypeDef hspi1;
 extern DMA_HandleTypeDef hdma_spi1_tx;
 
 ws2812b_color ledBuffer[PIXELS];
-#define zero 0b11000000
-#define one 0b11111000
 
 Status status;
-
-union Command {
-	uint8_t data[PIXELS * 3 + 1];
-	struct {
-		uint8_t pixels;
-		ws2812b_color colors[PIXELS];
-	};
-}__attribute__((packed));
-
 Command command;
 int rxPos;
 
-void setPixels() {
+void copyPixelsToBuffer() {
 	if (command.pixels > PIXELS) {
 		command.pixels = 0;
 		return;
@@ -38,7 +27,7 @@ void setPixels() {
 	status.hasUpdate = 0;
 }
 
-uint8_t buffer[24 * PIXELS + 10];
+uint8_t spiBuffer[24 * PIXELS + 10];
 
 extern "C" void run() {
 	rxPos = 0;
@@ -47,47 +36,49 @@ extern "C" void run() {
 	status.hasUpdate = 0;
 	status.lastRx = HAL_GetTick();
 	status.receiving = 0;
-	float p = 0;
 	LL_USART_TransmitData8(USART1, '!');
 
 	while (1) {
-		p += 0.1;
+		if (HAL_DMA_GetState(&hdma_spi1_tx) != HAL_DMA_STATE_BUSY) {
+			continue;
+		}
 		if (status.hasUpdate) {
 			LL_USART_DisableIT_RXNE (USART1);
-			setPixels();
+			copyPixelsToBuffer();
 			LL_USART_EnableIT_RXNE(USART1);
-			int b = 0;
+			int bit = 0;
 			for (int led = 0; led < status.maxPixels; led++) {
 				for (int i = 7; i >= 0; i--) {
-					buffer[b++] = ledBuffer[led].green & (1 << i) ? one : zero;
+					spiBuffer[bit++] = ledBuffer[led].green & (1 << i) ? WS_ONE : WS_ZERO;
 				}
 				for (int i = 7; i >= 0; i--) {
-					buffer[b++] = ledBuffer[led].red & (1 << i) ? one : zero;
+					spiBuffer[bit++] = ledBuffer[led].red & (1 << i) ? WS_ONE : WS_ZERO;
 				}
 				for (int i = 7; i >= 0; i--) {
-					buffer[b++] = ledBuffer[led].blue & (1 << i) ? one : zero;
+					spiBuffer[bit++] = ledBuffer[led].blue & (1 << i) ? WS_ONE : WS_ZERO;
 				}
 			}
-			buffer[b] = 0;
+			spiBuffer[bit] = 0;
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-			HAL_SPI_Transmit_DMA(&hspi1, buffer, 3 * 8 * status.maxPixels + 1);
+			HAL_SPI_Transmit_DMA(&hspi1, spiBuffer, 3 * 8 * status.maxPixels + 1);
 		} else {
 			if (HAL_DMA_GetState(&hdma_spi1_tx) != HAL_DMA_STATE_BUSY) {
 				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 			}
 		}
-//		HAL_Delay(8);
 	}
 }
 
 extern "C" void rxHandle() {
 	if (LL_USART_IsActiveFlag_ORE (USART1)) {
+		// overrun
 		LL_USART_ClearFlag_ORE(USART1);
 		rxPos = 0;
 		status.hasUpdate = 0;
 		return;
 	}
 	if (LL_USART_IsActiveFlag_RXNE (USART1)) {
+		// has data
 		auto now = HAL_GetTick();
 		if (rxPos > 0 && now - status.lastRx > 2) {
 			rxPos = 0;
